dd509537fb405261ce867c6f22b1d3b5d1a89b28
fishlinghu <fishlinghu@gmail.com>
2016-11-25 15:21:48 -0500

    Finish file output of the emit()

--- src/mr_tasks.h
+++ src/mr_tasks.h
@@ -2,6 +2,9 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 /* CS6210_TASK Implement this data structureas per your implementation.
 		You will need this when your worker is running the map task*/
@@ -26,6 +29,15 @@ inline BaseMapperInternal::BaseMapperInternal() {
 /* CS6210_TASK Implement this function */
 inline void BaseMapperInternal::emit(const std::string& key, const std::string& val) {
 	std::cout << "Dummy emit by BaseMapperInternal: " << key << ", " << val << std::endl;
+	ofstream file;
+    file.open ("output_file_name", ios_base::out | ios_base::app); 
+    // filename should be unique for each worker
+    // so we should assign each worker with an unique ID
+    // in that way, we dont need to communicate with the master to tell it the path of temporary file
+    // the master can find the file using, for example, "2.txt", and 2 is the id of a certain worker
+    file << key << " " << val << endl;
+
+    file.close();
 }
 
 
@@ -55,4 +67,9 @@ inline BaseReducerInternal::BaseReducerInternal() {
 /* CS6210_TASK Implement this function */
 inline void BaseReducerInternal::emit(const std::string& key, const std::string& val) {
 	std::cout << "Dummy emit by BaseReducerInternal: " << key << ", " << val << std::endl;
+	ofstream file;
+    file.open ("output_file_name", ios_base::out | ios_base::app); 
+    file << key << " " << val << endl;
+
+    file.close();
 }
